10948cfc3240a9bf596c86ea194867f71f7f5ad6
hirechrisf <hirechrisf@users.noreply.github.com>
2023-01-14 20:05:46 +0000

    deploy: 5913d94efdf2d1e7dd3ec25d7ae52c10b86c6a22

---
+++ _next/static/rVvxu9aRnPMLIMNYURBt4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-f179c677fea849bf.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
