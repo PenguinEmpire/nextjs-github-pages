5913d94efdf2d1e7dd3ec25d7ae52c10b86c6a22
cfehring <hirechrisf@gmail.com>
2023-01-14 12:05:02 -0800

    Updates to index.js
    just to test triggering github actions

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>Discover and deploy Vercel boilerplate example Next.js projects.</p>
           </a>
 
           <a
@@ -45,23 +45,13 @@ export default function Home() {
           >
             <h2>Deploy &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public URL at Vercel.
             </p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
       </footer>
     </div>
   );
